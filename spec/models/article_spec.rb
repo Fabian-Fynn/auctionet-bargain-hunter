912e673129bc80f474e54062a001caeb7dbc1daa
Fabian Johannes Hoffmann <fhoffmann.mmt-b2013@fh-salzburg.ac.at>
2015-04-22 21:27:21 +0200

    specs(article_spec): test for API response

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,7 +3,12 @@ require 'json'
 
 describe Article do
   context 'validate article model' do
-    it 'should validate API response' do
+    it 'retrieves data from API' do
+      query = 'per_page=12'
+      article_list = Article.get_articles(query)
+
+      expect(article_list).to be_kind_of(Array)
+      expect(article_list.length).to eq(12)
     end
   end
 end
