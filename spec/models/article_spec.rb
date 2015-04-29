617c132e276de27912dd4e8d16f0d3940864e245
Fabian Johannes Hoffmann <fhoffmann.mmt-b2013@fh-salzburg.ac.at>
2015-04-29 21:40:57 +0200

    specs(article_spec): check Article creation

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 require 'json'
 
-describe Article do
-  context 'validate article model' do
+describe "In general", Article do
+  context ', while API processing' do
     it 'retrieves data from API' do
       query = "per_page=12"
       article_list = Article.get_articles(query)
@@ -15,6 +15,23 @@ describe Article do
       query = "per_page=12&force_response_code=410"
       article_list = Article.get_articles(query)
       expect(article_list).to eq(410)
+    end
+  end
+
+end
+
+describe "An Instance of", Article do
+  before :each do
+    @article = Article.create(item_id: 33)
+  end
+
+  context "when new" do
+    it "should be properly initialized" do
+      expect(@article).to be_a(Article)
+    end
+
+    it "has an item_id" do
+      expect(@article.item_id).to eq(33)
     end
   end
 end
